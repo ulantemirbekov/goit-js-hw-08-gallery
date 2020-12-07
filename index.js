83ba1d3a78ab2e409b74b9b67c210b5b930a5ee5
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-12-07 13:23:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const lightboxRef = document.querySelector('.js-lightbox');
 const lightboxOverlayRef = document.querySelector('.lightbox__overlay');
 const lightboxImageRef = document.querySelector('.lightbox__image');
 const lightboxCloseRef = document.querySelector('[data-action="close-lightbox"]');
+let indexCurrentImage;
 
 
 const createGallery = galleryArray.reduce((acc, image) => {
@@ -23,12 +24,14 @@ function openWideImage(event) {
     event.preventDefault();
     if (event.target.nodeName !== 'IMG') { return };
 
+    indexCurrentImage = Number(event.target.dataset.index);
     lightboxRef.classList.add('is-open');
     lightboxImageRef.src = event.target.dataset.source;
     lightboxImageRef.alt = event.target.alt;
 
     lightboxCloseRef.addEventListener('click', closeWideImage);
     lightboxOverlayRef.addEventListener('click', closeWideImage);
+    window.addEventListener('keydown', onPressKey);
 };
 
 galleryRef.addEventListener('click', openWideImage);
@@ -37,4 +40,35 @@ galleryRef.addEventListener('click', openWideImage);
 function closeWideImage() {
     lightboxRef.classList.remove('is-open');
     lightboxImageRef.src = '';
+
+    lightboxCloseRef.removeEventListener('click', closeWideImage);
+    lightboxOverlayRef.removeEventListener('click', closeWideImage);
+    window.removeEventListener('keydown', onPressKey);
+}
+
+
+function onPressKey(event) {
+    switch (event.code) {
+
+        case 'Escape':
+            closeWideImage();
+            break;
+
+        // case 'ArrowRight':
+        //     indexCurrentImage + 1 === galleryArray.length
+        //         ? (indexCurrentImage = 0)
+        //         : (indexCurrentImage += 1);
+        //     lightboxImageRef.src = galleryArray[indexCurrentImage].original;
+        //     break;
+
+        // case 'ArrowLeft':
+        //     indexCurrentImage === 0
+        //         ? (indexCurrentImage = galleryArray.length - 1)
+        //         : (indexCurrentImage -= 1);
+        //     lightboxImageRef.src = galleryArray[indexCurrentImage].original;
+        //     break;
+
+        default:
+            break;
+    }
 }
